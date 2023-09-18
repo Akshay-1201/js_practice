8cffd5a22a1efb6b4be578f0041812ac4ec0ebc8
Akshay-1201 <akshaykute12@gmail.com>
2023-09-18 15:06:49 +0530

    commit

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,3 +1,20 @@
-document.querySelector(".center").addEventListener("mousemove", function(det){
-    console.log(det.clientX,det.clientY);
-})
+const throttleFunction=(func, delay)=>{ 
+    let prev = 0;
+    return (...args) => {
+      let now = new Date().getTime();
+      console.log(now-prev, delay);
+      if(now - prev> delay){
+        prev = now;
+        return func(...args); 
+      }
+    }
+};
+document.querySelector(".center").addEventListener("mousemove",throttleFunction((dets) => {
+    let div = document.createElement("div");
+    div.classList.add('imagediv');
+    div.style.left = dets.clientX + 'px';
+    div.style.top = dets.clientY + 'px';
+
+    document.body.appendChild(div);
+}, 400)
+);
