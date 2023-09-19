c11474d1bfc2dfdaf71cc62039650f8e3a52d40a
Akshay-1201 <akshaykute12@gmail.com>
2023-09-19 12:16:36 +0530

    commit

--- Project 1/script.js
+++ Project 1/script.js
@@ -2,7 +2,6 @@ const throttleFunction=(func, delay)=>{
     let prev = 0;
     return (...args) => {
       let now = new Date().getTime();
-      console.log(now-prev, delay);
       if(now - prev> delay){
         prev = now;
         return func(...args); 
@@ -15,6 +14,13 @@ document.querySelector(".center").addEventListener("mousemove",throttleFunction(
     div.style.left = dets.clientX + 'px';
     div.style.top = dets.clientY + 'px';
 
+    let img = document.createElement("img");
+    img.setAttribute("src", "https://images.unsplash.com/photo-1694807865565-70252084fa27?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80")
+
+    div.appendChild(img);
     document.body.appendChild(div);
+    setTimeout(function(){
+      div.remove();
+    },2000)
 }, 400)
 );
